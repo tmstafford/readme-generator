2f55236372a2ff423ba138eb29a8545bba23d163
tmstafford <tatumstafford1@gmail.com>
2020-11-16 18:36:16 -0600

    initial generate markdown template

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,38 @@
 // function to generate markdown for README
-//function generateMarkdown(data) {
-  //return `# ${data.title}
+function generateMarkdown(data) {
+  return `
+  # ${data.title}
 
-//`;
-//}
+  ## Description
+  ${data.description}
+
+  ## Table Of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${data.license}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  ${data.username}
+  If you have additional questions, please contact: ${data.email}
+`;
+}
 
 // module.exports = generateMarkdown;
