d364bd1683f7e8e3d458634f37baf8deccd4b050
tmstafford <tatumstafford1@gmail.com>
2020-11-16 18:42:53 -0600

    psuedocode functions

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 // array of questions for user
 const questions = [
@@ -69,7 +70,10 @@ const questions = [
 
 // function to initialize program
 //function init() {
-
+    // inquirer.prompt(questions)
+    // .then(data => {
+    //    return writeToFile(data);
+    //})
 //}
 
 // function call to initialize program
